547c93d923f1acb33dd8e36a4504c96ae30ef083
swallehrashid <swallehmtawazo@gmail.com>
2025-12-18 03:38:44 +0300

    prints the fibonacci sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = []
+    a, b = 0, 1
+
+    while len(fibonacci) < length:
+        fibonacci.append(a)
+        a, b = b, a + b
+
+    print(fibonacci)
